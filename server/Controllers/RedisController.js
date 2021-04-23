63bf7776d3f3547290e24a8909b6588f5d9cc2e8
TopWebFullDev <topwebfulldev@gmail.com>
2021-04-24 01:30:04 +0300

    pre-populate education and experience data

--- server/Controllers/RedisController.js
+++ server/Controllers/RedisController.js
@@ -3,6 +3,39 @@ let client = redis.createClient({
   host: process.env.REDIS_SERVER
 }) // Create a new redis instance
 
+client.on("connect", function() {
+  client.hmset('experience:0', [
+    'dateFrom', '2018-03-01',
+    'dateTo', '2020-11-01',
+    'company', "TrueProdigy, USA",
+    'role', "Full-stack developer",
+    'description', "Work at Prodigy company as a Full-stack developer (MERN stack) and React-Native developer for Hybrid Mobile\ndevelopment. This was a big project and we were able to finish it successfully."
+  ])
+  client.hmset('experience:1', [
+    'dateFrom', '2016-11-01',
+    'dateTo', '2018-02-01',
+    'company', "Snowfall, UK",
+    'role', "DevOps, Backend developer",
+    'description', "Python is all the rage in the world and its frameworks are rapidly improving and releasing. I worked as Backend developer using Python frameworks(Flask/Django) and After that, I participated in developing Artificial Intelligence part."
+  ])
+  client.hmset('experience:2', [
+    'dateFrom', '2014-10-01',
+    'dateTo', '2016-10-01',
+    'company', "Gudiz",
+    'role', "Web developer",
+    'description', "As a major full-stack developer, I have worked with many projects from scratch to the end including ERP, CMS, Odoo,\nCRM using Javascript frameworks and Python."
+  ])
+
+  client.hmset('education:0', [
+    'yearFrom', '2009',
+    'yearTo', '2014',
+    'school', "Moscow University",
+    'skills', "[\"Python\",\"React/Redux\",\"Angular\",\"Vue\",\"Svelte\",\"Elixir\",\"Flask/Django\",\"PHP\",\"SQL&NoSQL\"]"
+  ])
+  
+
+});
+
 exports.get_all_experiences = (req, res, next) => {
   let return_dataset = []
 
